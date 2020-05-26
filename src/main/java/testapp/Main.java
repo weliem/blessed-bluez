628cd2903c2365af0920b3d5b6eb949ab821a754
Martijn van Welie <martijn.van.welie@philips.com>
2020-05-26 21:01:21 +0200

    Adding logger config

--- src/main/java/testapp/Main.java
+++ src/main/java/testapp/Main.java
@@ -1,6 +1,24 @@
 package testapp;
 
+import blessed.HBLogger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+
 public class Main {
+
+    private static final String TAG = "APP";
+
+    static {
+        InputStream stream = Main.class.getClassLoader().
+                getResourceAsStream("logging.properties");
+        try {
+            LogManager.getLogManager().readConfiguration(stream);
+        } catch (IOException e) {
+            HBLogger.e(TAG, "Could not load logging.properties");
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World");
