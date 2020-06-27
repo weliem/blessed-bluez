30fe020930d28f46762432d988e88bb1a32d52ef
Martijn van Welie <martijn.van.welie@philips.com>
2020-06-27 07:31:59 +0200

    Adding test

---
+++ blessed/src/test/java/com/welie/blessed/BluetoothCentralTest.java
@@ -0,0 +1,13 @@
+package com.welie.blessed;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BluetoothCentralTest {
+
+    @Test
+    void firstTest() {
+        assertEquals(1,1);
+    }
+}
