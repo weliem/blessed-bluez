5cd4db4b8c564caea69e06a8aa1d9a9221c9e241
martijn <martijn.van.welie@gmail.com>
2020-07-05 20:18:16 +0200

    Cleanup Handler class

--- blessed/src/main/java/com/welie/blessed/internal/Handler.java
+++ blessed/src/main/java/com/welie/blessed/internal/Handler.java
@@ -1,12 +1,8 @@
 package com.welie.blessed.internal;
 
-import com.welie.blessed.internal.Looper;
-import com.welie.blessed.internal.Message;
-import com.welie.blessed.internal.MessageQueue;
-
 public class Handler {
 
-    private MessageQueue messageQueue = new MessageQueue();
+    private final MessageQueue messageQueue = new MessageQueue();
     private final Looper looper;
 
     public Handler(String name) {
@@ -20,6 +16,7 @@ public class Handler {
         }
     }
 
+    @SuppressWarnings("UnusedReturnValue")
     public final boolean post(Runnable r) {
         return this.postDelayed(r, 0);
     }
@@ -31,19 +28,11 @@ public class Handler {
         return sendRunnableAtTime(r, System.currentTimeMillis() + delayMillis);
     }
 
-    private final boolean sendRunnableAtTime(Runnable r, long targetTime) {
+    private boolean sendRunnableAtTime(Runnable r, long targetTime) {
         return messageQueue.enqueueMessage(new Message(r, targetTime));
-
     }
 
     public final void removeCallbacks(Runnable r) {
         messageQueue.removeMessages(r);
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-//        System.out.println("finalize handler");
-        super.finalize();
-        looper.stop();
     }
 }
