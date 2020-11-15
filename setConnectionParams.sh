92a1ce794dc5cc9e0c46fa927b3071c31e80443d
Martijn van Welie <martijn.van.welie@philips.com>
2020-11-15 09:11:17 +0100

    Adding scripts for viewing and setting connection parameters

---
+++ setConnectionParams.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+chmod 777 /sys/kernel/debug
+chmod 666 /sys/kernel/debug/bluetooth/hci0/conn_min_interval
+chmod 666 /sys/kernel/debug/bluetooth/hci0/conn_max_interval
+chmod 666 /sys/kernel/debug/bluetooth/hci0/supervision_timeout
+echo 6 > /sys/kernel/debug/bluetooth/hci0/conn_min_interval
+echo 12 > /sys/kernel/debug/bluetooth/hci0/conn_max_interval
+echo 200 > /sys/kernel/debug/bluetooth/hci0/supervision_timeout
+
+
---
+++ viewConnectionParams.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cat /sys/kernel/debug/bluetooth/hci0/conn_min_interval
+cat /sys/kernel/debug/bluetooth/hci0/conn_max_interval
+cat /sys/kernel/debug/bluetooth/hci0/supervision_timeout
+
+
