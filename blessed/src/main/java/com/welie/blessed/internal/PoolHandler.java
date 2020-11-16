a0fcdc3927bc49fdbecbd88c65e0ea104544621a
Martijn van Welie <martijn.van.welie@philips.com>
2020-11-16 09:51:48 +0100

    Create new PoolHandler class that is based on an Executor

---
+++ blessed/src/main/java/com/welie/blessed/internal/PoolHandler.java
@@ -0,0 +1,48 @@
+package com.welie.blessed.internal;
+
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class PoolHandler {
+
+    private final Executor executor ;
+    private final Map<Runnable, Timer> delayedRunnables = new ConcurrentHashMap<>();
+
+    public PoolHandler(String name) {
+        executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> Thread.currentThread().setName(name));
+    }
+
+    public void stop() {
+    }
+
+    public final void post(Runnable runnable) {
+        executor.execute(runnable);
+    }
+
+    public final void postDelayed(Runnable runnable, long delayMillis) {
+       Timer timer = new Timer();
+       delayedRunnables.put(runnable, timer);
+       timer.schedule(new TimerTask() {
+           @Override
+           public void run() {
+               delayedRunnables.remove(runnable);
+               post(runnable);
+           }
+       }, delayMillis);
+    }
+
+    public final void removeCallbacks(Runnable runnable) {
+        Timer timer = delayedRunnables.get(runnable);
+        if (timer != null) {
+            timer.cancel();
+            delayedRunnables.remove(runnable);
+        }
+    }
+
+
+}
