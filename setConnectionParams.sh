2f9f70f66d56e878feaaf2aaf24eccdb6d280d70
Martijn van Welie <martijn.van.welie@philips.com>
2021-01-24 09:15:13 +0100

    Update connection parameters

--- setConnectionParams.sh
+++ setConnectionParams.sh
@@ -4,7 +4,7 @@ chmod 666 /sys/kernel/debug/bluetooth/hci0/conn_min_interval
 chmod 666 /sys/kernel/debug/bluetooth/hci0/conn_max_interval
 chmod 666 /sys/kernel/debug/bluetooth/hci0/supervision_timeout
 echo 6 > /sys/kernel/debug/bluetooth/hci0/conn_min_interval
-echo 12 > /sys/kernel/debug/bluetooth/hci0/conn_max_interval
+echo 24 > /sys/kernel/debug/bluetooth/hci0/conn_max_interval
 echo 200 > /sys/kernel/debug/bluetooth/hci0/supervision_timeout
 
 
