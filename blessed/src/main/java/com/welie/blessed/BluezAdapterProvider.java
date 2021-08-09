100d0cb097b0d436a2109010e6735cea1708c934
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2021-08-09 10:20:35 +1000

    Gracefully handle no DBus.

--- blessed/src/main/java/com/welie/blessed/BluezAdapterProvider.java
+++ blessed/src/main/java/com/welie/blessed/BluezAdapterProvider.java
@@ -6,26 +6,33 @@ import org.freedesktop.dbus.connections.impl.DBusConnection;
 import org.freedesktop.dbus.exceptions.DBusException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
 class BluezAdapterProvider {
 
+    private static final String TAG = BluezAdapterProvider.class.getSimpleName();
+    private final Logger logger = LoggerFactory.getLogger(TAG);
+
     public final @Nullable DBusConnection dBusConnection;
     public final @Nullable BluezAdapter adapter;
 
     public BluezAdapterProvider() {
-        DBusConnection tempConnection = null;
+        DBusConnection tempConnection;
         try {
             tempConnection = DBusConnection.newConnection(DBusConnection.DBusBusType.SYSTEM);
 
             // Make sure the thread pool is 1, so that we are sure that message arrive in the order they were sent
             tempConnection.changeThreadCount((byte) 1);
         } catch (DBusException e) {
-            e.printStackTrace();
+            logger.error("Dbus connection failed", e);
+            dBusConnection = null;
+            adapter = null;
+            return;
         }
         dBusConnection = tempConnection;
         this.adapter = chooseBluezAdapter(DbusHelper.findBluezAdapters(dBusConnection));
@@ -34,6 +41,7 @@ class BluezAdapterProvider {
 
     /**
      * Pick the adapter with the highest index, if there are more than one
+     *
      * @param adapters list of adapters
      * @return the chosen BluezAdapter or null if adapters was empty
      */
